5416759cc72716ed5447f611826e26c878f6072b
Exclusiveideas <muftaudeenoluwatosin@gmail.com>
2022-04-06 06:59:11 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
+import { useRef, useEffect } from 'react';
 import './App.css';
+import * as faceapi from "face-api.js";
 
 function App() {
+  const videoRef = useRef();
+  const canvasRef = useRef();
+
+  useEffect(() => {
+    startVideo();
+
+    videoRef && loadModels();
+
+  }, [])
+  
+    const loadModels = () => {
+      Promise.all([
+        faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+        faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+        faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+        faceapi.nets.faceExpressionNet.loadFromUri('/models'),
+      ]).then(() => {
+        handleVideo();
+      })
+    }
+
+  const startVideo = () => {
+    navigator.mediaDevices.getUserMedia({ video: true })
+      .then((currentStream) => {
+        videoRef.current.srcObject = currentStream;
+      })
+      .catch((err) => {
+        console.error(err)
+      });
+  }
+
+  const handleVideo = async () => {
+    setInterval(async() => {
+      const detections = await faceapi.detectAllFaces(videoRef.current, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions();
+      
+      canvasRef.current.innerHtml = faceapi.createCanvasFromMedia(videoRef.current);
+      faceapi.matchDimensions(canvasRef.current, {
+        width: 940,
+        height: 650,
+      })
+
+      const resized = faceapi.resizeResults(detections, {
+        width: 940,
+        height: 650,
+      });
+
+      // canvasRef.getContext("2d").clearRect(0, 0, canvasRef.width, canvasRef.height)
+      faceapi.draw.drawDetections(canvasRef.current, resized)
+      faceapi.draw.drawFaceLandmarks(canvasRef.current, resized)
+      faceapi.draw.drawFaceExpressions(canvasRef.current, resized)
+
+    }, 1000)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ display: 'flex', width: '100vw', height: '100vh', flexDirection: 'column', alignItems: 'center', justifyContent: 'space-between'}} className="App">
+      <h1>FACE API DETECTION</h1>
+      <div style={{display: 'flex',  alignItems: 'center', flexDirection: 'column'}}>
+        <video crossOrigin='anonymous' ref={videoRef} id="video" autoPlay ></video>
+      </div>
+        <canvas ref={canvasRef} width="940" height="650" style={{ position: 'absolute', top: '100px'}} />
+      
     </div>
   );
 }
